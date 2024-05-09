c4c47a4d44acd913bed43a0c6f90a230a962fa49
Anton Piskunov <antonpiskunov140@gmail.com>
2024-05-09 16:57:00 +0300

    feat: add file button

--- chat.php
+++ chat.php
@@ -1,9 +1,11 @@
 <?php 
+/*
   session_start();
   include_once "php/config.php";
   if(!isset($_SESSION['unique_id'])){
     header("location: login.php");
   }
+  */
 ?>
 <?php include_once "header.php"; ?>
 <body>
@@ -11,6 +13,7 @@
     <section class="chat-area">
       <header>
         <?php 
+        /*
           $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
           // Выбираем пользователя из базы данных по его идентификатору
           $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
@@ -19,21 +22,23 @@
           }else{
             header("location: users.php"); // Если пользователь не найден, перенаправляем на страницу с пользователями
           }
+          */
         ?>
         <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-        <img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <img src="php/images/<?php //echo $row['img']; ?>" alt="">
         <div class="details">
-          <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
-          <p><?php echo $row['status']; ?></p>
+          <span><?php //echo $row['fname']. " " . $row['lname'] ?></span>
+          <p><?php //echo $row['status']; ?></p>
         </div>
       </header>
       <div class="chat-box">
 
       </div>
       <form action="#" class="typing-area">
-        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+        <button class="btn__file"><i class="fa fa-paperclip"></i></button>
+        <input type="text" class="incoming_id" name="incoming_id" value="<?php //echo $user_id; ?>" hidden>
         <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
-        <button><i class="fab fa-telegram-plane"></i></button>
+        <button class="btn__send"><i class="fab fa-telegram-plane"></i></button>
       </form>
     </section>
   </div>
